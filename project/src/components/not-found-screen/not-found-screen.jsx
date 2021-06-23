dbd176fe654e8095f2799da22a998d83b2a26c13
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-06-23 22:38:22 +0500

    Добавляет компоненты страниц приложения

---
+++ project/src/components/not-found-screen/not-found-screen.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function NotFoundScreen() {
+  return (
+    <div className="page page--error">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link header__logo-link--active">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="#">
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__login">Sign in</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <main className="page__main page__main--error">
+        <div className="page__error-container container">
+          <section className="error error--404">
+            <div className="error__text">
+              <h1 className="error__title">404. Page Not Found</h1>
+              <a className="error__link" href='/'>Back to home</a>
+            </div>
+          </section>
+        </div>
+      </main>
+      <footer className="footer">
+        <a className="footer__logo-link" href="/">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
