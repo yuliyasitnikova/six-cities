2a9af5be0517f7ee3b815f308281fdb4b7c63d38
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-06-27 19:02:45 +0500

    Добавляет компонент HeaderNav

--- project/src/components/cities-screen/cities-screen.jsx
+++ project/src/components/cities-screen/cities-screen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import HeaderNav from '../header-nav/header-nav';
 import CitiesPlacesList from '../cities-places-list/cities-places-list';
 import CitiesPlaceProp from '../cities-place/cities-place.prop';
 
@@ -16,22 +17,7 @@ function CitiesScreen(props) {
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
               </a>
             </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+            <HeaderNav />
           </div>
         </div>
       </header>
