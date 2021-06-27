68fc52104532a71767fde31f568f768d5fe9f4cc
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-06-27 18:42:40 +0500

    Добавляет моковые данные для отображения списка предложений

---
+++ project/src/mocks/offers.js
@@ -0,0 +1,44 @@
+const offers = [
+  {
+    id: 1,
+    isPremium: true,
+    isFavorite: false,
+    previewImage: 'img/apartment-01.jpg',
+    price: 120,
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+    rating: 4,
+  },
+  {
+    id: 2,
+    isPremium: false,
+    isFavorite: true,
+    previewImage: 'img/room.jpg',
+    price: 80,
+    title: 'Wood and stone place',
+    type: 'Private room',
+    rating: 5,
+  },
+  {
+    id: 3,
+    isPremium: false,
+    isFavorite: false,
+    previewImage: 'img/apartment-02.jpg',
+    price: 132,
+    title: 'Canal View Prinsengracht',
+    type: 'Apartment',
+    rating: 4,
+  },
+  {
+    id: 4,
+    isPremium: true,
+    isFavorite: false,
+    previewImage: 'img/apartment-03.jpg',
+    price: 180,
+    title: 'Nice, cozy, warm big bed apartment',
+    type: 'Apartment',
+    rating: 5,
+  },
+];
+
+export default offers;
