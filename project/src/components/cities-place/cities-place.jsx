f99b92812c83deb21c19886619229c475135a4a2
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-06-27 18:45:13 +0500

    Использует данные в карточке предложения

--- project/src/components/cities-place/cities-place.jsx
+++ project/src/components/cities-place/cities-place.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import CitiesPlaceProp from './cities-place.prop';
 
-function CitiesPlace() {
+function CitiesPlace(props) {
+  const {offer} = props;
+  const {isPremium, isFavorite, previewImage, price, title, type, rating} = offer;
+  const ratingValue = `${20 * rating}%`;
+
   return (
     <article className="cities__place-card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
+      {isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ''}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image"/>
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className={isFavorite ? 'place-card__bookmark-button place-card__bookmark-button--active button' : 'place-card__bookmark-button button'} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
@@ -26,17 +29,21 @@ function CitiesPlace() {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: '80%'}}></span>
+            <span style={{width: ratingValue}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
 }
+
+CitiesPlace.propTypes = {
+  offer: CitiesPlaceProp,
+};
 
 export default CitiesPlace;
