15c2bc4cdd552b48c03d7799efb139978c7318a3
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-07-15 12:59:09 +0500

    Выносит сортировку в отдельный компонент

--- project/src/components/places/places.jsx
+++ project/src/components/places/places.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {PlacesListClassModifier} from '../../const';
+import Sorting from '../sorting/sorting';
 import PlacesList from '../places-list/places-list';
 import PlacesMap from '../places-map/places-map';
 import placesItemProp from '../places-item/places-item.prop';
@@ -20,21 +21,7 @@ function Places({city, places}) {
       <section className="cities__places places">
         <h2 className="visually-hidden">Places</h2>
         <b className="places__found">{places.length} place{places.length !== 1 && 's'} to stay in {city}</b>
-        <form className="places__sorting" action="#" method="get">
-          <span className="places__sorting-caption">Sort by</span>
-          <span className="places__sorting-type" tabIndex="0">
-            Popular
-            <svg className="places__sorting-arrow" width="7" height="4">
-              <use xlinkHref="#icon-arrow-select" />
-            </svg>
-          </span>
-          <ul className="places__options places__options--custom places__options--opened">
-            <li className="places__option places__option--active" tabIndex="0">Popular</li>
-            <li className="places__option" tabIndex="0">Price: low to high</li>
-            <li className="places__option" tabIndex="0">Price: high to low</li>
-            <li className="places__option" tabIndex="0">Top rated first</li>
-          </ul>
-        </form>
+        <Sorting />
         <PlacesList className={PlacesListClassModifier.CITIES} places={places} onMouseEnterCallback={onPlaceMouseEnter} onMouseLeaveCallback={onPlaceMouseLeave} />
       </section>
       <div className="cities__right-section">
