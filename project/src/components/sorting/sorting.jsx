db6e35ab1d0d7cb37c82d646152cfaf0bca2af38
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-07-15 14:40:06 +0500

    Реализует отображение и скрытие выпадающего списка сортировки

--- project/src/components/sorting/sorting.jsx
+++ project/src/components/sorting/sorting.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import {connect} from 'react-redux';
@@ -6,20 +6,27 @@ import {ActionCreator} from '../../store/actions';
 import {SortType} from '../../const';
 
 function Sorting({sortType, onChangeSort}) {
-  const onOptionsClick = ({target}) => {
+  const [openedStatus, setOpenedStatus] = useState(false);
+
+  const onTypeClick = useCallback(() => {
+    setOpenedStatus(!openedStatus);
+  }, []);
+
+  const onOptionsClick = useCallback(({target}) => {
     onChangeSort(target.textContent);
-  };
+    setOpenedStatus(false);
+  }, []);
 
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex="0">
+      <span className="places__sorting-type" tabIndex="0" onClick={onTypeClick}>
         {sortType}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select" />
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened" onClick={onOptionsClick}>
+      <ul className={classNames('places__options', 'places__options--custom', {'places__options--opened' : openedStatus})} onClick={onOptionsClick}>
         {Object.values(SortType).map((type) => (
           <li key={`Sort by ${type}`} className={classNames('places__option', {'places__option--active' : sortType === type})} tabIndex="0">{type}</li>
         ))}
