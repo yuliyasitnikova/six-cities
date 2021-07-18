54bee2b76905e5c93abf59890fb7e3d8d2a74aca
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-07-18 16:17:21 +0500

    Добавляет проверку авторизации в компоненте HeaderNav

--- project/src/components/header-nav/header-nav.jsx
+++ project/src/components/header-nav/header-nav.jsx
@@ -1,35 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../const';
+import {isAuth} from '../../utils';
 
-function HeaderNav() {
+function HeaderNav({authorizationStatus}) {
   return (
     <nav className="header__nav">
       <ul className="header__nav-list">
-        <li className="header__nav-item user">
-          <Link className="header__nav-link header__nav-link--profile" to={AppRoute.LOGIN}>
-            <div className="header__avatar-wrapper user__avatar-wrapper">
-            </div>
-            <span className="header__login">Sign in</span>
-          </Link>
-        </li>
-      </ul>
-      <ul className="header__nav-list">
-        <li className="header__nav-item user">
-          <Link className="header__nav-link header__nav-link--profile" to={AppRoute.FAVORITES}>
-            <div className="header__avatar-wrapper user__avatar-wrapper">
-            </div>
-            <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-          </Link>
-        </li>
-        <li className="header__nav-item">
-          <a className="header__nav-link" href="#">
-            <span className="header__signout">Sign out</span>
-          </a>
-        </li>
+        {isAuth(authorizationStatus)
+          ? (
+            <>
+              <li className="header__nav-item user">
+                <Link className="header__nav-link header__nav-link--profile" to={AppRoute.FAVORITES}>
+                  <div className="header__avatar-wrapper user__avatar-wrapper">
+                  </div>
+                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                </Link>
+              </li>
+              <li className="header__nav-item">
+                <a className="header__nav-link" href="#">
+                  <span className="header__signout">Sign out</span>
+                </a>
+              </li>
+            </>
+          )
+          : (
+            <li className="header__nav-item user">
+              <Link className="header__nav-link header__nav-link--profile" to={AppRoute.LOGIN}>
+                <div className="header__avatar-wrapper user__avatar-wrapper">
+                </div>
+                <span className="header__login">Sign in</span>
+              </Link>
+            </li>
+          )}
       </ul>
     </nav>
   );
 }
 
-export default HeaderNav;
+HeaderNav.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: state.authorizationStatus,
+});
+
+export {HeaderNav};
+export default connect(mapStateToProps, null)(HeaderNav);
