05865d8e8639fcb1200ab532fb6ed28a7f618014
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-07-24 21:49:38 +0500

    Динамически выводит звезды рейтинга в форме

--- project/src/components/reviews-form/reviews-form.jsx
+++ project/src/components/reviews-form/reviews-form.jsx
@@ -1,12 +1,21 @@
 import React, {useState} from 'react';
 import {COMMENT_MIN_LENGTH, COMMENT_MAX_LENGTH} from '../../const';
 
+const ratingValues = [
+  {value: 5, title: 'perfect'},
+  {value: 4, title: 'good'},
+  {value: 3, title: 'not bad'},
+  {value: 2, title: 'badly'},
+  {value: 1, title: 'terribly'},
+];
+
 function ReviewsForm() {
   const [rating, setRating] = useState(null);
   const [comment, setComment] = useState('');
   const isButtonDisabled = (rating === null || comment.length < COMMENT_MIN_LENGTH);
 
-  const handleRatingChange = ({target}) => setRating(target.value);
+
+  const handleRatingChange = ({target}) => setRating(Number(target.value));
 
   const handleCommentChange = ({target}) => {
     const value = target.value;
@@ -24,40 +33,16 @@ function ReviewsForm() {
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
 
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" checked={rating === '5'} onChange={handleRatingChange} />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" checked={rating === '4'} onChange={handleRatingChange} />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" checked={rating === '3'} onChange={handleRatingChange} />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" checked={rating === '2'} onChange={handleRatingChange} />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" checked={rating === '1'} onChange={handleRatingChange} />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
+        {ratingValues.map(({title, value}) => (
+          <React.Fragment key={title}>
+            <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio" checked={value === rating} onChange={handleRatingChange} />
+            <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title={title}>
+              <svg className="form__star-image" width="37" height="33">
+                <use xlinkHref="#icon-star" />
+              </svg>
+            </label>
+          </React.Fragment>
+        ))}
       </div>
 
       <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={comment} onChange={handleCommentChange} />
