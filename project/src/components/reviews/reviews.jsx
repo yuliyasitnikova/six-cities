20a976a6f51e0a39eded3ca3ae8496bac5f3edc1
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-07-24 21:58:27 +0500

    Отображает форму добавления комментария для авторизованных пользователей

--- project/src/components/reviews/reviews.jsx
+++ project/src/components/reviews/reviews.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {isAuth} from '../../utils';
 import ReviewsList from '../reviews-list/reviews-list';
 import ReviewsForm from '../reviews-form/reviews-form';
 import reviewsItemProp from '../reviews-item/reviews-item.prop';
 
-function Reviews({reviews}) {
+function Reviews({reviews, authorizationStatus}) {
   return (
     <section className="property__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ReviewsList reviews={reviews} />
-      <ReviewsForm />
+      {isAuth(authorizationStatus) && <ReviewsForm />}
     </section>
   );
 }
 
 Reviews.propTypes = {
   reviews: PropTypes.arrayOf(reviewsItemProp),
+  authorizationStatus: PropTypes.string.isRequired,
 };
 
-export default Reviews;
+const mapStateToProps = (state) => ({
+  authorizationStatus: state.authorizationStatus,
+});
+
+export {Reviews};
+export default connect(mapStateToProps, null)(Reviews);
