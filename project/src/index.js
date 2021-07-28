c9d84b42ebb6c076ed4fe706923f61918ccae51c
Юлия Ситникова <yu.sitnikova@inbox.ru>
2021-07-28 22:51:38 +0500

    Подключает Redux Toolkit

--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
 import {createAPI} from './api';
 import rootReducer from './store/root-reducer';
 import {redirect} from './store/middlewares/redirect';
@@ -12,13 +10,16 @@ import App from './components/app/app';
 
 const api = createAPI();
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk.withExtraArgument(api)),
-    applyMiddleware(redirect),
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => (
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }).concat(redirect)
   ),
-);
+});
 
 store.dispatch(checkAuth());
 store.dispatch(fetchPlaces());
